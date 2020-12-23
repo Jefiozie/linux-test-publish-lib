214a3a5df7d098469571cace6a54bb174dbadff7
Jefiozie <jefiozie.bosch@gmail.com>
2020-12-23 21:08:01 +0100

    add dist files

---
+++ dist/libs/publish-2/src/lib/publish-2.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.publish2 = void 0;
+function publish2() {
+    return 'publish-2';
+}
+exports.publish2 = publish2;
+//# sourceMappingURL=publish-2.js.map
